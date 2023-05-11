f06588eaed0a3888cd658afd2f0536f402ecd40f
Anatolii Pokhylko <pokhylko@gmail.com>
2023-05-11 17:12:51 +0300

    feat: create scoreboard page

---
+++ src/pages/Scoreboard/Scoreboard.tsx
@@ -0,0 +1,73 @@
+import { FC } from 'react'
+
+import './Scoreboard.scss'
+import { Button } from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
+
+type Props = {
+  name: string
+  score: number
+}
+
+type Rating = {
+  name: string
+  date: string
+  score: number
+}
+
+const MAX_LINES = 10
+
+export const Scoreboard: FC<Props> = ({ name, score }) => {
+  const navigate = useNavigate()
+  const date = new Date().toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  })
+  const saved = localStorage.getItem('score') || '[]'
+  const rating: Rating[] = JSON.parse(saved)
+
+  rating.push({ name, date, score })
+
+  const newRating = rating.sort((a, b) => b.score - a.score)
+
+  if (newRating.length > MAX_LINES) {
+    newRating.length = MAX_LINES
+  }
+
+  localStorage.setItem('score', JSON.stringify(newRating))
+
+  return (
+    <div className="scoreboard">
+      <h2>Scoreboard</h2>
+      <table className="scoreboard__table">
+        <thead className="scoreboard__table-head">
+          <tr>
+            {['Rank', 'Name', 'Date', 'Score'].map(el => (
+              <th className="scoreboard__table-cell" key={el}>
+                {el}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="scoreboard__table-body">
+          {newRating.map((rating, index) => (
+            <tr key={`${rating.name}+${rating.score}`}>
+              <td className="scoreboard__table-cell scoreboard__table-cell--ranked">
+                {index + 1}
+              </td>
+              <td className="scoreboard__table-cell">{rating.name}</td>
+              <td className="scoreboard__table-cell">{rating.date}</td>
+              <td className="scoreboard__table-cell">{rating.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="scoreboard__buttons">
+        <Button onClick={() => navigate('/')}>To Welcome Screen</Button>
+        <Button onClick={() => navigate('/main')}>Play!</Button>
+      </div>
+    </div>
+  )
+}
